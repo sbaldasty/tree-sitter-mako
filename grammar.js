b8db3a0b1368555a253270f051b7864c11dcbf5d
Steven Baldasty <sbaldasty@gmail.com>
2025-07-23 22:35:08 -0400

    links to language spec

--- grammar.js
+++ grammar.js
@@ -31,17 +31,30 @@ module.exports = grammar({
     _ws_req: $ => /\s+/,
 
     attribute: $ => seq($.identifier, $._ws_opt, '=', $._ws_opt, $.string, $._ws_opt),
+
+    // See https://docs.makotemplates.org/en/latest/syntax.html#comments
     comment: $ => seq('##', repeat(/[^\n]/)),
+
     identifier: $ => /[A-Za-z_][0-9A-Za-z_]*/,
+
+    // See https://docs.makotemplates.org/en/latest/syntax.html#include
     include_block: $ => seq('<%include', $._ws_req, repeat($.attribute), choice('/>', seq('>', '</%include>'))),
+
+    // See https://docs.makotemplates.org/en/latest/syntax.html#inherit
     inherit_block: $ => seq('<%inherit', $._ws_req, repeat($.attribute), choice('/>', seq('>', '</%inherit>'))),
+
+    // See https://docs.makotemplates.org/en/latest/syntax.html#module-level-blocks
     module_block: $ => seq('<%!', $._ws_req, $.injected_python_block, '%>'),
 
     // See https://docs.makotemplates.org/en/latest/syntax.html#newline-filters
     newline_filter: $ => seq('\\', '\n'),
 
+    // See https://docs.makotemplates.org/en/latest/syntax.html#namespace
     namespace_block: $ => seq('<%namespace', $._ws_req, repeat($.attribute), choice('/>', seq('>', '</%namespace>'))),
+
+    // See https://docs.makotemplates.org/en/latest/syntax.html#python-blocks
     python_block: $ => seq('<%', $._ws_req, $.injected_python_block, '%>'),
+
     string: $ => choice(seq('"', repeat(/[^"]/), '"'), seq("'", repeat(/[^']/), "'"))
 
 	    /*
