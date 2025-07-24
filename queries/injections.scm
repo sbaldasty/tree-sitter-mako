4a9b6424f488ecc7d9fb98d877b42e31a3fef944
Steven Baldasty <sbaldasty@gmail.com>
2025-07-24 11:49:47 -0400

    restore html and python injection

---
+++ queries/injections.scm
@@ -0,0 +1,6 @@
+((injected_html) @injection.content
+ (#set! injection.language "html")
+ (#set! injection.combined))
+
+((injected_python_block) @injection.content
+ (#set! injection.language "python"))
