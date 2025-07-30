7143919ed2633437bd37768c52f4ca10ebcb2bc0
Steven Baldasty <sbaldasty@gmail.com>
2025-07-30 17:59:34 -0400

    better python end detection

--- src/scanner.c
+++ src/scanner.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include <tree_sitter/parser.h>
 #include <wctype.h>
 
@@ -47,16 +49,92 @@ static bool scan_injected_html(TSLexer *lexer) {
 
 static bool scan_injected_python_block(TSLexer *lexer) {
   bool any_chars = false;
+  bool in_comment = false;
+  bool in_apostro_str = false;
+  bool in_quote_str = false;
+  bool in_apostro_ml_str = false;
+  bool in_quote_ml_str = false;
+  bool escaping = false;
+  int quotes = 0;
+  int apostros = 0;
   while (lexer->lookahead != 0) {
+	  printf("ch=%c, apos=%d\n", lexer->lookahead, apostros);
     if (any_chars) {
       lexer->mark_end(lexer);
     }
-    if (lexer->lookahead == '%') {
+    if (in_comment) {
+      in_comment = lexer->lookahead != '\n';
+      lexer->advance(lexer, false);
+    } else if (in_apostro_str) {
+      if (apostros > 0 && lexer->lookahead == '\'' && !escaping) {
+        apostros++;
+      } else {
+        apostros = 0;
+      }
+      in_apostro_str = lexer->lookahead != '\n' && !(lexer->lookahead == '\'' && !escaping);
+      escaping = !escaping && lexer->lookahead == '\\';
+      lexer->advance(lexer, false);
+    } else if (in_quote_str) {
+      if (quotes > 0 && lexer->lookahead == '"' && !escaping) {
+        quotes++;
+      } else {
+        quotes = 0;
+      }
+      in_quote_str = lexer->lookahead != '\n' && !(lexer->lookahead == '"' && !escaping);
+      escaping = !escaping && lexer->lookahead == '\\';
+      lexer->advance(lexer, false);
+    } else if (in_apostro_ml_str) {
+	    printf("Found apostro ml\n");
+      if (lexer->lookahead == '\'') {
+        apostros++;
+      } else {
+        apostros = 0;
+      }
+      if (apostros == 3) {
+        apostros = 0;
+	in_apostro_ml_str = false;
+      }
+      lexer->advance(lexer, false);
+    } else if (in_quote_ml_str) {
+      if (lexer->lookahead == '"') {
+        quotes++;
+      } else {
+        quotes = 0;
+      }
+      if (quotes == 3) {
+        quotes = 0;
+	in_quote_ml_str = false;
+      }
+      lexer->advance(lexer, false);
+    } else if (lexer->lookahead == '#') {
+      lexer->advance(lexer, false);
+      in_comment = true;
+    } else if (lexer->lookahead == '\'') {
+      lexer->advance(lexer, false);
+      if (apostros == 2) {
+        in_apostro_ml_str = true;
+	apostros = 0;
+      } else {
+        apostros++;
+        in_apostro_str = true;
+      }
+    } else if (lexer->lookahead == '"') {
+      lexer->advance(lexer, false);
+      if (quotes == 2) {
+        in_quote_ml_str = true;
+	quotes = 0;
+      } else {
+        in_quote_str = true;
+        quotes++;
+      }
+    } else if (lexer->lookahead == '%') {
       lexer->advance(lexer, false);
       if (lexer->lookahead == '>') {
         return any_chars;
       }
     } else {
+      apostros = 0;
+      quotes = 0;
       lexer->advance(lexer, false);
     }
     any_chars = true;
